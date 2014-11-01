72378e781d366c02cccdc03fe4296a57a9281702
hkmix <cybersach@gmail.com>
2014-11-01 17:40:18 -0400

    Update

---
+++ monster_db.py
@@ -0,0 +1,18 @@
+class Mons(object):
+    NAME = 0
+    LVL = 1
+    HP = 2
+    ATK = 3
+    DEF = 4
+    ACC = 5
+    AVO = 6
+    CHAR = 7
+    VIS = 8
+    COL = 9
+    EXP = 10
+    DROP = 11
+
+    M = \
+        {
+            'player':['engineer', 0, 100, ]
+        }
