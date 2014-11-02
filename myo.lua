8df951ed62fd1f097bd2a0b3aa4202057d1f3abc
hkmix <cybersach@gmail.com>
2014-11-02 13:36:16 -0500

    Tweak Myo script

--- myo.lua
+++ myo.lua
@@ -12,15 +12,16 @@ PITCH_DOWN=0
 INITIALIZE=0
 
 function onForegroundWindowChange(app, title)
-    myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
-	if (title=="Rogue@UW - ENGHack 2014" or title=="Debug Console") then
+	if (title=="Rogue@UW - ENGHack 2014" or title=="Debug Console" or title=="Myo Script Manager") then
+		if (INITIALIZE==0 and title=="Debug Console") then
+			myo.debug("Make the finger spread gesture centered both up and down, and left and right.")
+		end
 		return true
 	end
 	return false
 end
 
 function onPoseEdge(pose, edge)
-    myo.debug("onPoseEdge: " .. pose .. ", " .. edge)
 	if (pose=="fingersSpread" and edge=="on") then
 		if (INITIALIZE==0) then
 			for i=0,10,1 do
@@ -30,25 +31,36 @@ function onPoseEdge(pose, edge)
 			PITCH_CENTER=PITCH_CENTER/10
 			YAW_CENTER=YAW_CENTER/10+3
 			INITIALIZE=1
+			myo.debug("Make the finger spread gesture in the up and right position.")
 		elseif (INITIALIZE==1) then
 			for i=0,10,1 do
 				PITCH_UP=PITCH_UP+myo.getPitch()
 				YAW_RIGHT=YAW_RIGHT+myo.getYaw()
 			end
 			PITCH_UP=PITCH_UP/10
-			YAW_RIGHT=YAW_RIGHT/10+3
+			YAW_RIGHT=YAW_RIGHT/10
+			if (YAW_RIGHT < 0) then
+				YAW_RIGHT = math.pi-YAW_RIGHT
+			end
+			YAW_RIGHT = YAW_RIGHT+3
 			INITIALIZE=2
+			myo.debug("Make the finger spread gesture in the down and left position.")
 		elseif (INITIALIZE==2) then
 			for i=0,10,1 do
 				PITCH_DOWN=PITCH_DOWN+myo.getPitch()
 				YAW_LEFT=YAW_LEFT+myo.getYaw()
 			end
 			PITCH_DOWN=PITCH_DOWN/10
-			YAW_LEFT=YAW_LEFT/10+3
+			YAW_LEFT=YAW_LEFT/10
+			if (YAW_LEFT < 0) then
+				YAW_LEFT = math.pi-YAW_LEFT
+			end
+			YAW_LEFT = YAW_LEFT+3
 			INITIALIZE=3
+			myo.debug("Your Myo is now calibrated :)")
 		elseif (INITIALIZE==3) then
 			myo.debug("\nPitch Up: "..PITCH_UP.."\nPitch Center: "..PITCH_CENTER.."\nPitch Down: "..PITCH_DOWN)
-			myo.debug("\nYaw Right: "..YAW_RIGHT.."\nYaw Center: "..YAW_CENTER.."\nYaw Left: "..YAW_LEFT)
+			myo.debug("\nYaw Right: "..YAW_RIGHT.."\nYaw Center: "..YAW_CENTER.."\nYaw Left: "..YAW_LEFT.."\n")
 			myo.keyboard("comma", "press")
 		end
 	elseif (pose=="waveIn" and edge=="on") then
@@ -58,7 +70,7 @@ end
 
 function onPeriodic()
 	CTIME = CTIME + 1
-    if (CTIME > CDELAY) then
+    if (CTIME > CDELAY and INITIALIZE==3) then
 		if(myo.getPitch() < PITCH_UP+math.abs((PITCH_UP+PITCH_CENTER)/2)/2) then
 			myo.keyboard("up_arrow", "press")
 			myo.debug("Up!")
@@ -67,11 +79,15 @@ function onPeriodic()
 			myo.keyboard("down_arrow", "press")
 			myo.debug("Down!")
 		end
-		if(myo.getYaw()+3 < YAW_RIGHT) then
+		yaw=myo.getYaw()
+		if (yaw<0) then
+			yaw = math.pi-yaw
+		end
+		if(yaw+3 < YAW_RIGHT) then
 			myo.keyboard("right_arrow", "press")
 			myo.debug("Right!")
 		end
-		if(myo.getYaw()+3 > YAW_LEFT) then
+		if(yaw+3 > YAW_LEFT) then
 			myo.keyboard("left_arrow", "press")
 			myo.debug("Left!")
 		end
